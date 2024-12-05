faf9585096c1b0c1d6ab4537f797abcd8b3ee082
Raunak Rose <raunakrode220cool@gmail.com>
2024-12-05 12:18:52 +0530

    compiled hardhat

---
+++ backendd/contracts/AssetTransfer.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AssetTransfer {
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function transfer(address to) public payable {
+        require(msg.sender != to, "Sender and recipient cannot be the same");
+        require(msg.value > 0, "Transfer amount must be greater than 0");
+        (bool sent, ) = to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+
+        emit Transfer(msg.sender, to, msg.value);
+    }
+}
---
+++ backendd/scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const AssetTransfer = await hre.ethers.getContractFactory("AssetTransfer");
+  const assetTransfer = await AssetTransfer.deploy();
+
+  await assetTransfer.deployed();
+
+  console.log(`AssetTransfer deployed to: ${assetTransfer.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
